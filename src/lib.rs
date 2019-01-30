b95f2cdb059a2892b4f58c0268a00110c0237056
Seb Ospina <kraige@gmail.com>
2019-01-30 20:45:37 +0100

    Implemented Default guitar for StringedElement

--- src/lib.rs
+++ src/lib.rs
@@ -13,20 +13,61 @@ enum FlexibleMaterial {
 /// Used to calculate when the next "note" in a scale should be length-wise 
 struct StringedElement {
     material: FlexibleMaterial,
+    /// The gauge of the string, in Millimeters
     gauge: f64,
     /// A vibrating string vibrates with a set of frequencies that depend on
     /// the string's tension. These frequencies can be derived from Newton's
     /// laws of motion. https://en.wikipedia.org/wiki/Tension_(physics)
-    frequency: f32,
+    /// The frequency is stored in Hertz
+    frequency: f64,
     enabled: bool,
+}
+
+impl StringedElement {
+    fn new_steel(gauge: f64, frequency: f64) -> StringedElement {
+        StringedElement {
+            material: FlexibleMaterial::Steel,
+            enabled: true,
+            gauge: gauge,
+            frequency: frequency,
+        }
+    }
+    fn new_wound(gauge: f64, frequency: f64) -> StringedElement {
+        StringedElement {
+            material: FlexibleMaterial::Wound,
+            enabled: true,
+            gauge: gauge,
+            frequency: frequency,
+        }
+    }
 }
 
 /// `Fretboard` a vector of strings to practice on
 struct Fretboard {
     strings: Vec<StringedElement>,
-    quantity: u8,
+    frets: u8,
 }
 
+/// `Default` provides a Guitar
+impl Default for Fretboard {
+    /// Gauge used is Regular Light
+    /// from https://en.wikipedia.org/wiki/String_(music)#Electric_guitar
+    /// Notes and Frequencies are taken from:
+    /// https://en.wikipedia.org/wiki/Guitar_tunings
+    fn default() -> Fretboard {
+        Fretboard {
+            strings: vec![
+                StringedElement::new_steel(0.2540, 329.63), // E4
+                StringedElement::new_steel(0.3302, 246.94), // B3
+                StringedElement::new_steel(0.4318, 196.00), // G3
+                StringedElement::new_wound(0.6604, 146.83), // D3
+                StringedElement::new_wound(0.9144, 110.00), // A2
+                StringedElement::new_wound(1.1684, 82.41),  // E2
+            ],
+            frets: 24, 
+        }
+    }
+}
 /// `FingerDigit` maintains the finger types
 enum FingerDigit {
     Thumb,
@@ -84,7 +125,7 @@ struct Hand {
     dexterity: u8,
 }
 
-/// 
+/// `Default` is the left hand setup
 impl Default for Hand{
     fn default() -> Hand {
         Hand{
