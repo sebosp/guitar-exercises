415ea9b6fac8d2f46ad04d6c829540e9fbc09778
Seb Ospina <kraige@gmail.com>
2019-02-05 19:56:48 +0100

    Added num_traits for randomizing scale patterns

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 
 extern crate pitch_calc;
 extern crate chords;
+extern crate rand;
+extern crate num_traits;
 
 pub mod fretboard;
 pub mod hand;
+pub mod scale;
 
 use fretboard::*;
 use hand::*;
+use scale::*;
 
 #[cfg(test)]
 mod test {
