d0a8bdd10463e2b4f70f6473262a5fd109c47cd3
Seb Ospina <kraige@gmail.com>
2019-02-05 20:07:40 +0100

    Implemented Random for Scale

--- src/scale.rs
+++ src/scale.rs
@@ -7,8 +7,11 @@ use pitch_calc::{
     Octave,
 };
 use chords::*;
+use rand::Rng;
+use num_traits::cast::FromPrimitive;
 
 /// `ScaleCategory` for Major, Minor, etc on a given note/pitch
+#[derive(Debug)]
 pub struct ScaleCategory {
     note: Letter,
     category: String
@@ -19,6 +22,20 @@ impl Default for ScaleCategory {
         ScaleCategory{
             note: Letter::C,
             category: "Major".to_string()
+        }
+    }
+}
+
+impl ScaleCategory {
+    pub fn random() -> ScaleCategory {
+        let mut rng = rand::thread_rng();
+        let scale_nth: usize = rng.gen();
+        let note_nth: i64 = rng.gen();
+        let scales = chords::supported_scales();
+        let total_scales = scales.len();
+        ScaleCategory{
+            note: Letter::from_i64(note_nth).unwrap(), // XXX: Can this actualy return None?
+            category: scales[scale_nth % total_scales].clone(),
         }
     }
 }
