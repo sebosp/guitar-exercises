0d61526076a3b07fcb95c2b6571b6f938269cd54
Seb Ospina <kraige@gmail.com>
2019-02-11 18:32:22 +0100

    Implemented debug, fixed Random

--- src/scale.rs
+++ src/scale.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+
+use chords::scale::*;
 use pitch_calc::{
     Hz,
     Letter,
@@ -9,18 +12,19 @@ use pitch_calc::{
 use chords::*;
 use rand::Rng;
 use num_traits::cast::FromPrimitive;
+use num_traits::cast::ToPrimitive;
 
 /// `ScaleCategory` for Major, Minor, etc on a given note/pitch
 #[derive(Debug)]
 pub struct ScaleCategory {
-    note: Letter,
-    category: String
+    category: String,
+    scale_notes: Vec<Letter>,
 }
 
 impl Default for ScaleCategory {
     fn default() -> ScaleCategory {
         ScaleCategory{
-            note: Letter::C,
+            scale_notes: vec![Letter::C],
             category: "Major".to_string()
         }
     }
@@ -29,13 +33,25 @@ impl Default for ScaleCategory {
 impl ScaleCategory {
     pub fn random() -> ScaleCategory {
         let mut rng = rand::thread_rng();
-        let scale_nth: usize = rng.gen();
-        let note_nth: i64 = rng.gen();
+        let scale_rng: usize = rng.gen();
         let scales = chords::supported_scales();
         let total_scales = scales.len();
+        let random_scale = &scales[scale_rng % total_scales];
+        let starting_note = Letter::from_i64(rng.gen_range(0, 12) as i64).unwrap();
+        println!("Working on scale for Letter: {:?}", starting_note);
+        let scale_notes:Vec<Letter> = chords::scale::get_scale(random_scale).iter().map(|offset|
+            Letter::from_i64(starting_note.to_i64().unwrap() + *offset as i64).unwrap()
+        ).collect();
         ScaleCategory{
-            note: Letter::from_i64(note_nth).unwrap(), // XXX: Can this actualy return None?
-            category: scales[scale_nth % total_scales].clone(),
+            scale_notes: scale_notes,
+            category: random_scale.clone(),
         }
+    }
+}
+
+
+impl fmt::Display for ScaleCategory {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {:?})", self.category, self.scale_notes)
     }
 }
