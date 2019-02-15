df2eaebd777a651f68e7fdce2ea852f2fc266325
Seb Ospina <kraige@gmail.com>
2019-02-15 19:46:55 +0100

    Implemented note_to_string

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,23 @@ use scale::*;
 
 pub fn note_to_string(note: pitch_calc::Letter) -> String {
     match note {
-        _ => "unimplemented".to_string()
+        pitch_calc::Letter::C   => " C".to_string(),
+        pitch_calc::Letter::Csh => "C#".to_string(),
+        pitch_calc::Letter::Db  => "Db".to_string(),
+        pitch_calc::Letter::D   => " D".to_string(),
+        pitch_calc::Letter::Dsh => "D#".to_string(),
+        pitch_calc::Letter::Eb  => "Eb".to_string(),
+        pitch_calc::Letter::E   => " E".to_string(),
+        pitch_calc::Letter::F   => " F".to_string(),
+        pitch_calc::Letter::Fsh => "F#".to_string(),
+        pitch_calc::Letter::Gb  => "Gb".to_string(),
+        pitch_calc::Letter::G   => " G".to_string(),
+        pitch_calc::Letter::Gsh => "G#".to_string(),
+        pitch_calc::Letter::Ab  => "Ab".to_string(),
+        pitch_calc::Letter::A   => " A".to_string(),
+        pitch_calc::Letter::Ash => "A#".to_string(),
+        pitch_calc::Letter::Bb  => "Bb".to_string(),
+        pitch_calc::Letter::B   => " B".to_string(),
     }
 }
 #[cfg(test)]
